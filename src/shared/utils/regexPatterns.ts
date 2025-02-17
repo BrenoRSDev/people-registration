c4a2434c7ad16d91f4ccfd0a7e78da6982432b9f
Breno Ramos <brenosouza520@gmail.com>
2025-02-16 23:55:22 -0300

    chore: adiciona arquivo de regex

---
+++ src/shared/utils/regexPatterns.ts
@@ -0,0 +1,5 @@
+export const CPF_REGEX = /^(\d{3}\.\d{3}\.\d{3}-\d{2})$/;
+export const CPF_MASK = "000.000.000-00";
+
+export const TELEFONE_REGEX = /^\(\d{2}\) \d{5}-\d{4}$/;
+export const TELEFONE_MASK = "(00) 00000-0000";
