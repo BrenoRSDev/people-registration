89a708196ea4e731c12f6d34b3fa40a1a9bfc2fc
Breno Ramos <brenosouza520@gmail.com>
2025-02-17 00:27:50 -0300

    feat: adiciona rotas para criação e edição de usuário

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "../pages";
+import { Home, User } from "../pages";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/user/create" element={<User />} />
+        <Route path="/user/edit/:id" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
