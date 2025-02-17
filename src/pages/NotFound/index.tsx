848c507e22b73a0027cdbb14698e9f270c2a52bd
Breno Ramos <brenosouza520@gmail.com>
2025-02-17 00:44:05 -0300

    feat: implementa NotFoundTemplate na página

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import {
+    NotFoundTemplate,
+    PageTemplate
+} from "../../shared/components/templates";
+  
+  function NotFound() {
+    return (
+      <PageTemplate>
+        <NotFoundTemplate />
+      </PageTemplate>
+    );
+  }
+  
+  export default NotFound;
+  
