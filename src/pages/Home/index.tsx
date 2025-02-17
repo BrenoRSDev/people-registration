3c73db16120fd7e8db515655f13159e24102939a
Breno Ramos <brenosouza520@gmail.com>
2025-02-16 21:51:06 -0300

    feat: implementa componente home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { PageTemplate, UserListTemplate } from "../../shared/components/templates";
+
+function Home() {
+  return (
+    <PageTemplate>
+      <UserListTemplate />
+    </PageTemplate>
+  );
+}
+
+export default Home;
